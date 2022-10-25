0ae39af0d84fadcf4521762358dfca18f6b72479
Alain Noel <alain.noel34@gmail.com>
2022-10-25 15:47:43 +0200

    add mock-pokemon

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello, Node !')
+const express = require('express')
+let pokemons = require('./mock-pokemon');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => res.send('Hello, Express 2 !'))
+
+app.get('/api/pokemons/:id', (req,res) => {
+  const id = req.params.id
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  res.send(`Vous avez demandé le pokémon n°${pokemon.name}.`)
+  })
+
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
