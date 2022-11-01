668b07294c2e1aaeec12099107e0e5cdbb7ffb19
Alain Noel <alain.noel34@gmail.com>
2022-11-01 15:25:55 +0100

    add app.put / modif pokemon

--- app.js
+++ app.js
@@ -31,11 +31,21 @@ app.get('/api/pokemons', (req, res) => {
   res.send(success(message, pokemons))
 })
 
-app.get('/api/pokemons/:id', (req,res) => {
+app.get('/api/pokemons/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const pokemon = pokemons.find(pokemon => pokemon.id === id)
   const message = 'Un pokemon a bien été trouvée.'
   res.json(success(message, pokemon))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonUpdated = { ...req.body, id: id }
+  pokemons = pokemons.map(pokemon => {
+    return pokemon.id === id ? pokemonUpdated : pokemon
+  })
+  const message = `Le pokémon ${pokemonUpdated.name} a bien été modifée.`
+  res.json(success(message, pokemonUpdated))
 })
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
